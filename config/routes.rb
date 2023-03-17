a4067b206136a305a11dfee5abf2cecc29abd267
CindyMisoi <crmisoi2k@gmail.com>
2023-03-17 11:46:21 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  get '/students', to: 'students#index'
+  # get all students
+  get '/students', to: 'students#index' 
+
+  # get student grades
   get '/students/grades', to: 'students#grades'
+
+  #get student with the highest grade
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  #get individual student => dynamic route
+  get '/students/:id', to: 'students#show'
+
 end
